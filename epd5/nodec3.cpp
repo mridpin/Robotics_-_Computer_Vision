bf73632ae3ed96f642db6f15bccb5c29c2586e77
Manuel Ridao Pineda <32127756+mridpin@users.noreply.github.com>
2018-05-15 17:16:21 +0200

    terminada epd5

--- epd5/nodec3.cpp
+++ epd5/nodec3.cpp
@@ -17,6 +17,34 @@
 #include "student.h"
 
 sensor_msgs::CameraInfo cameraCalibration;
+cv::Mat template_image;
+
+/**
+* OpenCV image coordinate frame: u corresponds to the column (and the X axis), v corresponds to the rows
+* of the image (and the Y axis)
+*/
+cv::Vec3f transfromToWorldCoordinates(float u, float v, float depth, sensor_msgs::CameraInfo calib)
+{
+	float X, Y, Z;
+	cv::Vec3f point3D;
+
+	//We apply the formulae
+
+	std::cout << "u: " << u << " v: " << v << " d: " <<  depth << std::endl;	
+
+	X=(u-calib.K[2])*depth/calib.K[0];
+	Y=(v-calib.K[5])*depth/calib.K[4];
+	Z=depth;
+
+	std::cout << "X: " << X << " Y: " << Y << " Z: " <<  Z << std::endl;
+
+	point3D[0]=X;
+	point3D[1]=Y;
+	point3D[2]=Z;
+	
+	return point3D;
+
+}
 
 void callback(const sensor_msgs::Image::ConstPtr& colorMsg, const sensor_msgs::Image::ConstPtr& depthMsg)
 {
@@ -30,7 +58,7 @@ void callback(const sensor_msgs::Image::ConstPtr& colorMsg, const sensor_msgs::I
     {
         bColor = cv_bridge::toCvShare(colorMsg, sensor_msgs::image_encodings::BGR8);
 	//The depth image comes as a image of 16 bit pixels, encoded as unsigned ints, and with the depth in millimeters
-        bDepth = cv_bridge::toCvCopy(depthMsg, sensor_msgs::image_encodings::TYPE_16UC1);
+        bDepth = cv_bridge::toCvCopy(depthMsg, sensor_msgs::image_encodings::TYPE_32FC1);
     }
     catch (cv_bridge::Exception& e)
     {
@@ -39,18 +67,22 @@ void callback(const sensor_msgs::Image::ConstPtr& colorMsg, const sensor_msgs::I
     }
 	bColor->image.copyTo(color);
 	//bDepth->image.copyTo(depth);
-	bDepth->image.convertTo(depth,CV_32F,1.0/1000.0); //Convert from int to float, and from millimeters to meters
+	bDepth->image.convertTo(depth,CV_32F,1.0); //Convert from int to float, and from millimeters to meters
 	
 	// Procesamiento de im�gen  y mapa de profundidad
-
 	processImageAndDepth(color, depth, out_frame);
+	//searchTemplate(color, template_image, out_frame);
+	// Moments
+	cv::Moments moment = moments(out_frame, false);
+	float x = moment.m10 / moment.m00;
+	float y = moment.m01 / moment.m00;
 
 	//As an example, we compute the 3D pose of the point (200,300) on the image
 	//Draw a circle at that position
-	cv::circle(color,cv::Point(200,300),10,cv::Scalar(0,0,255),3);
+	cv::circle(color,cv::Point(x,y),10,cv::Scalar(0,0,255),3);
 	//Transform that pixel to world coordinates using the depth from the Kinect
 	//The order of storage is (row, col), that is(300,200), but the coordinates are (col, row), that is (200,300)
-	transfromToWorldCoordinates(200,300,depth.at<float>(300,200),cameraCalibration);
+	//transfromToWorldCoordinates(200,300,depth.at<float>(300,200),cameraCalibration);
 
 	
 	
@@ -83,7 +115,7 @@ int main(int argc, char **argv)
    * part of the ROS system.
    */
 
-  ros::init(argc, argv, "epd4_c1");
+  ros::init(argc, argv, "epd5_c3");
 
 
 
@@ -115,7 +147,7 @@ int main(int argc, char **argv)
    * away the oldest ones.
    */
 
-    message_filters::Subscriber<sensor_msgs::Image> sub_image(n, "/camera/rgb/image_color", 1);
+    message_filters::Subscriber<sensor_msgs::Image> sub_image(n, "/camera/rgb/image_rect_color", 1);
     message_filters::Subscriber<sensor_msgs::Image> sub_disp(n, "/camera/depth_registered/image_raw", 1);
 
     // ApproximateTime takes a queue size as its constructor argument, hence MySyncPolicy(10)
@@ -125,6 +157,7 @@ int main(int argc, char **argv)
 
     ros::Subscriber calibration = n.subscribe("/camera/rgb/camera_info", 1, getCalibration); //subscribes to the Kinect video frames
 
+    template_image = cv::imread(argv[1]); //Read an image from file. 
 
     /**
     * ros::spin() will enter a loop, pumping callbacks.  With this version, all
